978e61c1dccfa89a712921d8493618172fd4fec9
son nguyen <sonnhfit@gmail.com>
2019-02-19 17:41:06 +0700

    add

--- apps/reports/views.py
+++ apps/reports/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Report
 # Create your views here.
 
 
 class ViewReport(View):
 
     def get(self, request):
+        Report.objects.create()
         return render(request, 'admin/create_report.html')
